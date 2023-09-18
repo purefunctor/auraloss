c44323d6ced2c1b3fa82934b5c2c0d127f8f55a5
Mike Solomon <mike@meeshkan.com>
2023-09-18 17:31:49 +0000

    Fixes files

--- examples/compressor/unsilence.py
+++ examples/compressor/unsilence.py
@@ -1,4 +1,7 @@
 import json
+import os
+D = [x for x in os.listdir('.') if ('o_' in x) and ('.wav' in x) and ('MX20' in x)]
+print(D)
 with open('silent_frames.json','r') as rf:
     silence = json.loads(rf.read())
     print(len(silence))
@@ -12,4 +15,4 @@ with open('silent_frames.json','r') as rf:
     TS = 0
     for x,y in O:
         TS += ((y-x)*512 // 16384)
-    print(TS)
+    
