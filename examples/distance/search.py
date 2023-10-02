5d2f76938f4c05a38137cf359171a856149b1dff
purefunctor <purefunctor@pm.me>
2023-10-02 08:58:48 +0000

    Add hyperparameter search script

---
+++ examples/distance/search.py
@@ -0,0 +1,51 @@
+from itertools import product
+from tcn import TCNModule
+from torchsummary import summary
+
+N_BLOCKS = range(4, 11)
+DILATION_GROWTH = [2, 3, 5, 10]
+KERNEL_SIZE = [5, 13, 15, 20, 25]
+CHANNEL_WIDTH = [32, 64, 96]
+
+configurations = []
+for nblocks, dilation_growth, kernel_size, channel_width in product(
+    N_BLOCKS, DILATION_GROWTH, KERNEL_SIZE, CHANNEL_WIDTH
+):
+    model = TCNModule(
+        nblocks=nblocks,
+        kernel_size=kernel_size,
+        dilation_growth=dilation_growth,
+        channel_width=channel_width,
+        stack_size=nblocks,
+    )
+    info = summary(model, verbose=0)
+    trainable_params = info.trainable_params
+    receptive_field = model.compute_receptive_field() / 44100 * 1000
+
+    if 200_000 > trainable_params > 100_000 and 100 < receptive_field < 1000:
+        print(
+            f"{nblocks=},{dilation_growth=},{kernel_size=},{channel_width=},{trainable_params},{receptive_field}ms"
+        )
+        configurations.append(
+            {
+                "nblocks": nblocks,
+                "dilation_growth": dilation_growth,
+                "kernel_size": kernel_size,
+                "channel_width": channel_width,
+                "trainable_params": trainable_params,
+            }
+        )
+
+print(f"Total: {len(configurations)}")
+
+
+with open("train.sh", "w") as f:
+    f.write("#!/usr/bin/sh")
+    f.write("\n\n")
+    for configuration in sorted(configurations, key=lambda configuration: configuration["trainable_params"]):
+        f.write(
+            "python train.py --nblocks={nblocks} --dilation_growth={dilation_growth} --kernel_size={kernel_size} --channel_width={channel_width}  # {trainable_params}".format(
+                **configuration
+            )
+        )
+        f.write("\n")
--- examples/distance/train.py
+++ examples/distance/train.py
@@ -1,92 +1,68 @@
+from argparse import ArgumentParser
 from data import DAY_1_FOLDER, DAY_2_FOLDER, DistanceAugmentDataModule
-from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
-from pytorch_lightning.loggers.wandb import WandbLogger
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.trainer import Trainer
 from tcn import TCNModule
 import torch
-import wandb
 
 torch.set_float32_matmul_precision("high")
 
-half = True
-if half:
-    precision = "16-mixed"
-else:
-    precision = "32-true"
+parser = ArgumentParser("training-script")
+parser.add_argument("--nblocks", help="Number of blocks")
+parser.add_argument("--dilation_growth", help="Dilation growth per block")
+parser.add_argument("--kernel_size", help="Kernel size per block")
+parser.add_argument("--channel_width", help="Channel width per block")
+parser.add_argument(
+    "--half", action="store_true", help="Use mixed-precision training", default=True
+)
 
-configuration = {
-    # "uTCN-100-C": {
-    #     "nblocks": 4,
-    #     "dilation_growth": 10,
-    #     "kernel_size": 5,
-    #     "causal": True,
-    #     "channel_width": 32,
-    #     "lr": 0.001,
-    # },
-    "uTCN-300-C": {
-        "nblocks": 4,
-        "dilation_growth": 10,
-        "kernel_size": 13,
-        "channel_width": 32,
-        "causal": True,
-        "lr": 0.001,
-    },
-    "uTCN-100-Cx3": {
-        "nblocks": 4,
-        "dilation_growth": 10,
-        "kernel_size": 5,
-        "channel_width": 96,
-        "causal": True,
-        "lr": 0.001,
-    },
-    "uTCN-300-Cx3": {
-        "nblocks": 4,
-        "dilation_growth": 10,
-        "kernel_size": 13,
-        "channel_width": 96,
-        "causal": True,
-        "lr": 0.001,
-    },
-    # "TCN-324-C": {
-    #     "nblocks": 10,
-    #     "dilation_growth": 2,
-    #     "kernel_size": 15,
-    #     "channel_width": 32,
-    #     "causal": True,
-    #     "lr": 0.001,
-    # },
-}
+configuration = parser.parse_args()
 
-for n, p in configuration.items():
-    model = TCNModule(**p)
-    datamodule = DistanceAugmentDataModule(
-        DAY_1_FOLDER,
-        DAY_2_FOLDER,
-        chunk_size=32768,
-        num_workers=16,
-        half=half,
-        batch_size=128,
-        near_is_input=True,
-    )
+nblocks = int(configuration.nblocks)
+dilation_growth = int(configuration.dilation_growth)
+kernel_size = int(configuration.kernel_size)
+channel_width = int(configuration.channel_width)
+half = configuration.half
+precision = "16-mixed" if half else "32-true"
 
-    wandb_logger = WandbLogger(project="near-to-far", name=f"{n}-128b", log_model="all")
-    wandb_logger.experiment.config.update({
+model = TCNModule(
+    nblocks=nblocks,
+    dilation_growth=dilation_growth,
+    kernel_size=kernel_size,
+    channel_width=channel_width,
+    lr=0.0001,
+)
+
+datamodule = DistanceAugmentDataModule(
+    DAY_1_FOLDER,
+    DAY_2_FOLDER,
+    chunk_size=32768,
+    num_workers=16,
+    half=half,
+    batch_size=128,
+    near_is_input=True,
+)
+
+wandb_logger = WandbLogger(
+    project="near-to-far",
+    name=f"TCN-{nblocks}n-{dilation_growth}g-{kernel_size}k-{channel_width}w",
+    log_model="all",
+)
+wandb_logger.experiment.config.update(
+    {
         "receptive_field": model.compute_receptive_field(),
         "batch_size": datamodule.batch_size,
         "chunk_size": datamodule.chunk_size,
-    })
+    }
+)
 
-    model_checkpoint = ModelCheckpoint(save_top_k=-1, every_n_epochs=1)
-    trainer = Trainer(
-        max_epochs=20,
-        callbacks=[model_checkpoint],
-        precision=precision,
-        logger=wandb_logger,
-    )
+model_checkpoint = ModelCheckpoint(save_top_k=-1, every_n_epochs=1)
+trainer = Trainer(
+    max_epochs=20,
+    callbacks=[model_checkpoint],
+    precision=precision,
+    logger=wandb_logger,
+)
 
-    trainer.fit(
-        model,
-        datamodule=datamodule,
-    )
-
-    wandb.finish()
+trainer.fit(model, datamodule=datamodule)
