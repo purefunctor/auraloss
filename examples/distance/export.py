d58636f1b06fa63767143ef778c9716f4bd820b1
Justin Garcia <purefunctor@gmail.com>
2023-10-04 13:43:20 +0800

    Use API for export script

--- examples/distance/export.py
+++ examples/distance/export.py
@@ -1,26 +1,28 @@
+from pathlib import Path
 from tcn import TCNModule
 import torch
 import wandb
 
+_exports = Path("exports")
+if not _exports.exists():
+    _exports.mkdir()
+
 api = wandb.Api()
 
-artifacts = [
-    "meeshkan/near-to-far/model-w2sg3k9w:v19",
-    "meeshkan/near-to-far/model-sdl0oecr:v19",
-    "meeshkan/near-to-far/model-t02dv6ru:v19",
-    "meeshkan/near-to-far/model-qj6nboda:v19",
-]
-
-for artifact in artifacts:
-    artifact = api.artifact(artifact)
+for run in api.runs(path="meeshkan/near-to-far"):
+    if len(run.tags) > 0 or run.state != "finished":
+        continue
+    artifact = api.artifact(f"meeshkan/near-to-far/model-{run.id}:latest")
     weights = artifact.get_path("model.ckpt").download("/tmp")
-    model = TCNModule.load_from_checkpoint(weights).eval()
+    model = TCNModule.load_from_checkpoint(
+        weights, map_location=torch.device("cpu")
+    ).eval()
     r = model.compute_receptive_field()
 
-    for s in [512, 1024, 2048]:
-        x = torch.rand((1, 1, s + r))
+    for s in [2, 4, 6, 8, 16]:
+        x = torch.rand((1, 1, 512 * s + r))
         model.to_onnx(
-            f"exports/{artifact.logged_by().name}-{s}.onnx",
+            f"exports/{run.name}-{512 * s}.onnx",
             x,
             input_names=["input"],
             output_names=["output"],
